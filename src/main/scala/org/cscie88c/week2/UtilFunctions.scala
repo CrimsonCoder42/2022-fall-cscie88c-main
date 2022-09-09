0587af34336695233b7d1038bfacfb08b9f4998b
CrimsonCoder42 <nostro37@gmail.com>
2022-09-09 13:10:41 -0400

    problem 1 , 1

--- src/main/scala/org/cscie88c/week2/UtilFunctions.scala
+++ src/main/scala/org/cscie88c/week2/UtilFunctions.scala
@@ -2,6 +2,7 @@ package org.cscie88c.week2
 
 object UtilFunctions extends App {
 
-  def maximum(a: Int, b: Int): Int =
+  def maximum(a: Int, b: Int): Int = if(a > b) a else b
 
+  println(maximum(6,5));
 }
