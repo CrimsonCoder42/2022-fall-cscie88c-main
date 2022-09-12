747b84e49028b2c87a909a039551bf71720bd608
CrimsonCoder42 <nostro37@gmail.com>
2022-09-12 11:38:34 -0400

    problem set 3 done

--- src/main/scala/org/cscie88c/week2/UniversityEmployee.scala
+++ src/main/scala/org/cscie88c/week2/UniversityEmployee.scala
@@ -4,8 +4,4 @@ class UniversityEmployee(val name: String, val email: String){
   def description: String = "Name: " + name + "," + " Email: " + email
 }
 
-object test extends App {
-  val uni = new UniversityEmployee("Devin", "nostro37@gmail.com")
 
-  println(uni.description)
-}
